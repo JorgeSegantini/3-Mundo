74af678e9b0da44bcb229e5e6b0b98e629cc7024
JorgeSegantini <101870090+JorgeSegantini@users.noreply.github.com>
2023-04-25 00:44:12 -0300

    Update exe72.py

--- exe72.py
+++ exe72.py
@@ -1,6 +1,11 @@
 numero = int(input('Digite um número entre 0 e 20: '))
-numerolista = ('Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezesssete', 'Dezoito', 'Dezenove', 'Vinte')
-if numero > 0 or numero <= 20:
-    print
-else:
-    numero = int(input('Tente novamente.'))
+numerolista = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezesssete', 'Dezoito', 'Dezenove', 'Vinte')
+for numero in range(0,20):
+    print(f'O número que voce digitou é {numero(numerolista[range])}')
+
+    #if numero in (0, 20):
+       # print(f'Você digitou o numero {numerolista[]}')
+    #elif numero == 1:
+        #print(numerolista[1])
+    #else:
+        #numero = int(input('Tente novamente.'))
