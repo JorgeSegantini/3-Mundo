0ecdaa16a9298f7b99a801a938d544f62d08cda3
JorgeSegantini <101870090+JorgeSegantini@users.noreply.github.com>
2023-04-25 22:05:21 -0300

    Update exe72.py

--- exe72.py
+++ exe72.py
@@ -1,7 +1,19 @@
-numero = int(input('Digite um número entre 0 e 20: '))
 numerolista = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezesssete', 'Dezoito', 'Dezenove', 'Vinte')
-for numero in range(0,20):
-    print(f'O número que voce digitou é {numero(numerolista[range])}')
+while True: 
+        numero = int(input('Digite um número entre 0 e 20: '))         
+        if numero <= 20 and numero > 0:
+            print(f'Eu vou comer {numerolista[numero]}')
+            ct = str(input('Deseja continuar [S/N] ')).upper()        
+        while ct in 'Ss':                      
+                print('', end='')
+        if ct in 'Nn':
+            print('Voce saiu do programa ')
+            break         
+        ct = str(input('Deseja continuar [S/N] ')).upper()        
+            
+
+#for numero in numerolista:        
+    #print(f'O número que voce digitou é {numerolista[numero]}')
 
     #if numero in (0, 20):
        # print(f'Você digitou o numero {numerolista[]}')
