61e68eb4be9d60370f084320978d669cbc881493
JorgeSegantini <101870090+JorgeSegantini@users.noreply.github.com>
2023-04-25 22:11:36 -0300

    Update exe72.py

--- exe72.py
+++ exe72.py
@@ -1,15 +1,17 @@
 numerolista = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezesssete', 'Dezoito', 'Dezenove', 'Vinte')
 while True: 
-        numero = int(input('Digite um número entre 0 e 20: '))         
-        if numero <= 20 and numero > 0:
-            print(f'Eu vou comer {numerolista[numero]}')
-            ct = str(input('Deseja continuar [S/N] ')).upper()        
-        while ct in 'Ss':                      
-                print('', end='')
-        if ct in 'Nn':
-            print('Voce saiu do programa ')
-            break         
-        ct = str(input('Deseja continuar [S/N] ')).upper()        
+    numero = int(input('Digite um número entre 0 e 20: '))         
+    while numero > 20 or numero < 0:
+        numero = int(input('Entrada invalida digite outro número entre 0 e 20: '))
+    print(f'Eu vou comer {numerolista[numero]}')
+    ct = str(input('Deseja continuar [S/N] ')).upper()
+    if ct in 'Ss':
+        print('vamos continuar')     
+    elif ct in 'Nn':
+        print('Voce saiu do programa ')
+        break         
+
+
             
 
 #for numero in numerolista:        
